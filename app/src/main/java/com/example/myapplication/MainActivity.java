e2976013dbcd32786e59f260ea29abae9f083e02
admin <enrique.gonzalez@educa.jcyl.es>
2021-12-20 17:29:19 +0100

    inicial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         *coordenadas de interes:
+         *
+         * campos de futbol: 41.666533584035875, -4.7194152409443175 (futbol)
+         * futbol sala: 41.6650419387921, -4.724735663752827 (futbol sala y baloncesto)
+         * piscina cubierta: 41.66581874238467, -4.72016801059828 (natacion)
+         * polideportivo: 41.66612503087524, -4.7173585988088975 (voleybol,natacion y tenis)
+         * */
     }
 }
